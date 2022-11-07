e557668deacb27fc11816540a21761700b0ae921
paperplane110 <1374736640@qq.com>
2022-11-07 19:18:00 +0800

    fix: 路由未响应

--- pages/article/index.vue
+++ pages/article/index.vue
@@ -5,7 +5,7 @@
       Article.
     </Text>
     <div class="grid grid-cols-2 gap-12 <sm:grid-cols-1">
-      <NuxtLink v-for="(article, idx) in data.reverse()" :key="idx" :to="article._path">
+      <NuxtLink v-for="(article, idx) in reverseData" :key="idx" :to="article._path">
         <!-- <div
           class="h-full border rounded-1rem overflow-clip bg-light-200 shadow-nsm transition-all duration-200 hover:(shadow-nlg)">
           <div class="h-200px bg-center bg-no-repeat bg-cover" :style="`background-image: url(${article.image.src})`">
@@ -16,17 +16,8 @@
             <Text base>{{ article.description }}</Text>
           </div>
         </div> -->
-        <div class="group ">
-          <div class="h-250px bg-center bg-no-repeat bg-cover rounded-1rem transition-all group-hover:(rounded-3rem)"
-            :style="`background-image: url(${article.image.src})`">
-          </div>
-          <p class="text-3xl font-thin">{{ article.title }}</p>
-          <div class="flex gap-4 transition-all">
-            <p class="code-family text-sm">{{ article.date }}</p>
-            <p class="text-gray-400 font-thin text-base">{{ article.description }}</p>
-          </div>
-
-        </div>
+        <ArticleFlatCard :title="article.title" :description="article.description" :date="article.date"
+          :img-src="article.image.src" />
       </NuxtLink>
       <br />
     </div>
@@ -37,10 +28,5 @@
 
 <script setup lang='ts'>
 const { data } = await useAsyncData('article', () => queryContent('/article').find())
+const reverseData = data.value.reverse()
 </script>
-
-<style scoped>
-.code-family {
-  font-family: 'JetBrains Mono', 'Courier New', Courier, monospace;
-}
-</style>
