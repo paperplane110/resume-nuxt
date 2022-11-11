c20c3bd0cd90f0b08a4758e2cb3c631740d668e3
paperplane110 <1374736640@qq.com>
2022-11-11 14:23:28 +0800

    Feat(gallery): refactor and add pics

---
+++ components/Gallery/Vpic.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="group flex items-center gap-4 mt-6">
+    <div
+      class="flex-[0.7] rounded-2rem overflow-hidden max-h-[100px] filter blur-sm transition-all duration-500 transform group-hover:(max-h-[1000px] filter-none)">
+      <img :src="imgInfo.src" :alt="imgInfo.alt" />
+    </div>
+    <div class="flex flex-col justify-between">
+      <div class="mt-2 pl-2 border-l-black border-l-2">
+        <Text sm>
+          {{ imgInfo.date }}
+        </Text>
+        <Text sm><b>{{ imgInfo.alt }}</b></Text>
+        <Text sm>{{ imgInfo.place }}</Text>
+        <Text sm class="text-gray-400">{{ imgInfo.detail }}</Text>
+      </div>
+    </div>
+  </div>
+</template>
+  
+<script setup lang='ts'>
+type horizontalPictureProps = {
+  src: string
+  alt: string
+  date: string
+  place: string
+  detail?: string
+}
+
+const imgInfo = defineProps<horizontalPictureProps>()
+</script>
+  
+<style scope>
+
+</style>
--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,43 +1,67 @@
 <template>
-    <div>
-        <br />
-        <Text hero>Gallery.</Text>
-        <Text>
-            From academic to impressionism, from photographs to 3D arts,
-            here collects all of my favorite artworks, photos, game scenes, and 3D arts.
-            Vibrant green, sweet pink, warmly orange, rustic black & white,
-            that's all about <Sparkle rainbow>life
-            </Sparkle>.
-        </Text>
-        <br />
-        <div v-for="(imgInfo, idx) in imgList" :key="idx" class="text-right">
-            <div class="rounded-2rem overflow-hidden mt-6 mb-2">
-                <img :src="imgInfo.src" :alt="imgInfo.alt" />
-            </div>
-            <div>
-                <Text sm>
-                    {{ imgInfo.date }} / <b>{{ imgInfo.alt }}</b> / {{ imgInfo.caption }}
-                </Text>
-                <Text sm class="text-gray-400">{{ imgInfo.detail }}</Text>
-            </div>
-        </div>
+  <div>
+    <br />
+    <Text hero>Gallery.</Text>
+    <Text>
+      这里汇集了所有我喜爱的作品：从学院派到印象派、从摄影到 3D 艺术。
+      生机勃勃的绿、甜美的粉、温暖的橙黄、冷静的蓝，克制的黑白，
+      这<Sparkle rainbow>一切的色彩</Sparkle>组成了我们生活的全部
+    </Text>
+    <Text class="text-gray-500">
+      From academic to impressionism, from photographs to 3D arts,
+      here collects all of my favorite artworks.
+      Vibrant green, sweet pink, warm orange, calm blue, rustic black & white,
+      that's all about <Sparkle rainbow>life
+      </Sparkle>.
+    </Text>
+    <br />
+    <div v-for="(imgInfo, idx) in imgList" :key="idx">
+      <GalleryVpic v-if="imgInfo.layout === 'v'" v-bind="imgInfo" />
+      <GalleryHpic v-else v-bind="imgInfo" />
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 const imgList = [
-    {
-        src: "/img/kidDog.jpeg",
-        alt: "The Kid and His Friend",
-        date: '2020.04',
-        caption: "Photoed at Bristol Brandon hill",
-    },
-    {
-        src: "/img/park.jpeg",
-        alt: "A Chair in Spring",
-        date: '2020.04',
-        caption: "Photoed at Bristol Brandon hill",
-        detail: "春日的布里斯托，落英缤纷，美不胜收"
-    }
+  {
+    src: "/img/gallery/2022_bj_red_leaf.jpeg",
+    alt: "地坛红叶(Red Leaves in Ditan Park)",
+    date: '2022.11',
+    place: "Photoed at Beijing, Ditan Park",
+  },
+  {
+    src: "/img/gallery/2022_bj_tree.jpeg",
+    alt: "秋日的地坛 (Ditan park in Autumn)",
+    date: '2022.11',
+    place: "Photoed at Beijing, Ditan Park",
+  },
+  {
+    src: "/img/gallery/2022_bj_child.jpeg",
+    alt: "孩子的地坛 (The Child in Ditan Park)",
+    date: '2022.11',
+    place: "Photoed at Beijing, Ditan Park",
+  },
+  {
+    src: "/img/gallery/2022_bj_ditan.jpeg",
+    alt: "画中的地坛 (Ditan park in the painting)",
+    date: '2022.11',
+    place: "Photoed at Beijing, the North Gate of Ditan Park",
+    detail: '人生海海，你我亦是画中人',
+    layout: 'v'
+  },
+  {
+    src: "/img/kidDog.jpeg",
+    alt: "The Kid and His Friend",
+    date: '2020.04',
+    place: "Photoed at Bristol Brandon hill",
+  },
+  {
+    src: "/img/park.jpeg",
+    alt: "A Chair in Spring",
+    date: '2020.04',
+    place: "Photoed at Bristol Brandon hill",
+    detail: "春日的布里斯托，落英缤纷，美不胜收"
+  },
 ]
 </script>
