7a9a73b3ee648223d2006dbf750d42c6d9acd473
paperplane110 <1374736640@qq.com>
2023-02-03 00:42:06 +0800

    Fix: wrong loading order

--- components/Image/index.vue
+++ components/Image/index.vue
@@ -1,14 +1,14 @@
 <template>
-  <div ref="wrapper" class="relative" @click="Loaded">
+  <div ref="wrapper" class="relative">
     <ImageBlur class="absolute top-0 left-0 transition-opacity duration-500"
       :class="isLoaded ? 'opacity-0' : 'opacity-100'" :hash="props.hash" :aspect-ratio="props.aspectRatio" />
     <img ref="image" class="absolute top-0 left-0 transition-opacity duration-500"
-      :class="isLoaded ? 'opacity-100' : 'opacity-0'" :alt="props.alt" />
+      :class="isLoaded ? 'opacity-100' : 'opacity-0'" :alt="props.alt" @load="Loaded" />
   </div>
 </template>
 
 <script setup lang='ts'>
-
+// Define props
 type imageProps = {
   src: string
   hash: string
@@ -18,14 +18,19 @@ type imageProps = {
 
 const props = defineProps<imageProps>()
 
-const wrapper = ref<HTMLDivElement>()
-const image = ref<HTMLImageElement>()
+// Whether img is loaded
 let isLoaded = ref(false)
 
 function Loaded() {
-  image.value.src = props.src
   isLoaded.value = true
-  console.log("img is loaded", isLoaded.value)
+}
+
+// Image lazy loading
+const wrapper = ref<HTMLDivElement>()
+const image = ref<HTMLImageElement>()
+
+function Entered() {
+  image.value.src = props.src
 }
 
 let observer: IntersectionObserver
@@ -33,7 +38,7 @@ let observer: IntersectionObserver
 onMounted(() => {
   observer = new IntersectionObserver((entries) => {
     if (entries[0].isIntersecting) {
-      Loaded()
+      Entered()
     }
   })
   observer.observe(wrapper.value)
