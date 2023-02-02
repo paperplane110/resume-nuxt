c0156d75ff19bc01acde2d01c4f1c2c080e72f90
paperplane110 <1374736640@qq.com>
2023-02-03 00:14:23 +0800

    Fix: onload is not working

--- components/Image/index.vue
+++ components/Image/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="relative" @click="Loaded">
+  <div ref="wrapper" class="relative" @click="Loaded">
     <ImageBlur class="absolute top-0 left-0 transition-opacity duration-500"
       :class="isLoaded ? 'opacity-0' : 'opacity-100'" :hash="props.hash" :aspect-ratio="props.aspectRatio" />
-    <NuxtImg class="absolute top-0 left-0 transition-opacity duration-500" loading="lazy"
-      :class="isLoaded ? 'opacity-100' : 'opacity-0'" :src="props.src" :alt="props.alt" @load="Loaded" />
+    <img ref="image" class="absolute top-0 left-0 transition-opacity duration-500"
+      :class="isLoaded ? 'opacity-100' : 'opacity-0'" :alt="props.alt" />
   </div>
 </template>
 
@@ -18,11 +18,29 @@ type imageProps = {
 
 const props = defineProps<imageProps>()
 
+const wrapper = ref<HTMLDivElement>()
+const image = ref<HTMLImageElement>()
 let isLoaded = ref(false)
 
 function Loaded() {
+  image.value.src = props.src
   isLoaded.value = true
   console.log("img is loaded", isLoaded.value)
 }
+
+let observer: IntersectionObserver
+
+onMounted(() => {
+  observer = new IntersectionObserver((entries) => {
+    if (entries[0].isIntersecting) {
+      Loaded()
+    }
+  })
+  observer.observe(wrapper.value)
+})
+
+onUnmounted(() => {
+  observer.disconnect()
+})
 
 </script>
