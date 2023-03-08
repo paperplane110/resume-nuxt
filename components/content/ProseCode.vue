8436bfe00f82bd5403617b5ea4a14286c840ade0
paperplane110 <1374736640@qq.com>
2023-03-08 15:51:21 +0800

    Fix: windicss screen var cause build failed

--- components/content/ProseCode.vue
+++ components/content/ProseCode.vue
@@ -34,20 +34,20 @@ export default defineComponent({
 <template>
   <Card class="my-6">
     <template #header v-if="isHeader">
-      <div class="flex-between px-4 py-2 rounded-t-1rem bg-dark-300 text-gray-300 <md:(py-1)">
+      <div class="flex justify-between items-center px-4 py-2 rounded-t-1rem bg-dark-300 text-gray-300">
         <div class="flex items-center gap-2 flex-1">
-          <div class="w-3 h-3 rounded-1 bg-red-500 <md:(w-2 h-2)"></div>
-          <div class="w-3 h-3 rounded-1 bg-yellow-500 <md:(w-2 h-2)"></div>
-          <div class="w-3 h-3 rounded-1 bg-green-500 <md:(w-2 h-2)"></div>
+          <div class="w-3 h-3 rounded-1 bg-red-500 "></div>
+          <div class="w-3 h-3 rounded-1 bg-yellow-500 "></div>
+          <div class="w-3 h-3 rounded-1 bg-green-500"></div>
         </div>
         <div class="flex-1 text-center">
           {{ filename ? filename : '' }}
         </div>
-        <div class="flex-1 text-right <md:(text-sm)">{{ language }}</div>
+        <div class="flex-1 text-right ">{{ language }}</div>
       </div>
     </template>
     <template #body>
-      <div class="px-4 py-2 bg-dark-700 overflow-auto <md:(text-sm)" :class="isRounded">
+      <div class="px-4 py-2 bg-dark-700 overflow-auto " :class="isRounded">
         <slot />
       </div>
     </template>
@@ -68,5 +68,13 @@ pre code .line.highlight {
 
 .dark pre code .line.highlight {
   @apply -mx-4 px-4 bg-dark-300 border-l-1 border-l-green-500;
+}
+
+@media (max-width: 768px) {
+
+  pre code .line {
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+  }
 }
 </style>
