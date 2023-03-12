04f5da11a8e69d29a4de0a969e82cc44044bf02b
paperplane110 <1374736640@qq.com>
2023-03-12 23:39:08 +0800

    Mode(ColorSwitch): made it sync

--- components/NavBar/ColorSwitch.vue
+++ components/NavBar/ColorSwitch.vue
@@ -2,9 +2,9 @@
   <Switch v-model="enabled" :class="enabled ? 'bg-gray-200 bg-opacity-30' : 'bg-gray-200'"
     class="relative inline-flex h-10 w-6 items-center rounded-full border border-gray-300 transition-color duration-500 hover:border-gray-500 dark:(border-gray-500 hover:border-gray-300)">
     <span class="sr-only">Enable notifications</span>
-    <span :class="enabled ? 'translate-y-2' : '-translate-y-2'"
+    <span :class="$colorMode.preference === 'dark' ? 'translate-y-2' : '-translate-y-2'"
       class="inline-block flex justify-center items-center ml-1px h-5 w-5 text-gray-500 transform rounded-full bg-white transition">
-      <font-awesome-icon v-if="enabled" icon="fa-line fa-moon" class="fa-xs" />
+      <font-awesome-icon v-if="$colorMode.preference === 'dark'" icon="fa-line fa-moon" class="fa-xs" />
       <font-awesome-icon v-else icon="fa-solid fa-sun" class="fa-xs" />
     </span>
   </Switch>
