1c4ff9eaf4a523c601d239bf3144a544312816c4
paperplane110 <1374736640@qq.com>
2023-05-25 23:58:07 +0800

    Mod(Navbar): update home button

--- components/NavBar/index.vue
+++ components/NavBar/index.vue
@@ -2,16 +2,9 @@
   <div class="h-18 w-full flex justify-between items-center">
     <div id="links" class="flex gap-4">
       <NuxtLink to="/">
-        <div id="icon" class="flex-center w-12 h-12 transition-all duration-200 filter" :class="homeActivatedStyles">
-          <svg width="30" height="30" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M100 50C100 59.8891 97.0675 69.5561 91.5735 77.7785C86.0794 86.001 78.2705 92.4096 69.1342 96.194C59.9979 99.9784 49.9445 100.969 40.2455 99.0393C30.5464 97.11 21.6373 92.348 14.6447 85.3553L50 50H100Z"
-              fill="#6481D1" />
-            <path
-              d="M100 0C100 6.56609 98.7067 13.0679 96.194 19.1342C93.6812 25.2005 89.9983 30.7124 85.3553 35.3553C80.7124 39.9983 75.2005 43.6812 69.1342 46.194C63.0679 48.7067 56.5661 50 50 50C43.4339 50 36.9321 48.7067 30.8658 46.194C24.7995 43.6812 19.2876 39.9983 14.6447 35.3553C10.0017 30.7124 6.31876 25.2004 3.80602 19.1342C1.29329 13.0679 -5.74026e-07 6.56609 0 0H100Z"
-              fill="#718DF3" fill-opacity="0.35" />
-            <path d="M50 0H100V50H50V0Z" fill="#3A67DB" fill-opacity="0.48" />
-          </svg>
+        <div class="group w-12 h-12 flex-center rounded-1rem transition-all transform active:(scale-95)">
+          <font-awesome-icon :icon="`fa-solid fa-circle-notch fa-xl`"
+            class="rotating text-2xl group-hover:(text-indigo-500)" />
         </div>
       </NuxtLink>
       <NavItemB icon="fa-blank" to="/playground" tooltip="???" :activated="routeActivated" />
@@ -44,3 +37,27 @@ const homeActivatedStyles = computed(() => {
   } else return ""
 })
 </script>
+
+<style scoped>
+@keyframes rotating {
+  from {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+
+  to {
+    -ms-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -webkit-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+
+.rotating {
+  animation: rotating 2s cubic-bezier(0.4, 0.82, 0.165, 1) infinite;
+}
+</style>
