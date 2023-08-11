442c2483fd03c98e1e8de68f7236a534852a3b62
Tianyu Yuan <62664574+paperplane110@users.noreply.github.com>
2023-08-11 02:22:07 +0000

    Fix(timer): typo and style

--- pages/timer.vue
+++ pages/timer.vue
@@ -68,7 +68,7 @@ const zeroToSixty = [
   "twenty", "twenty-one", "twenty-two", "twenty-three", "twenty-four", "twenty-five", "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine",
   "thirty", "thirty-one", "thirty-two", "thirty-hree", "thirty-four", "thirty-five", "thirty-six", "thirty-seven", "thirty-eight", "thirty-nine",
   "forty", "forty-one", "forty-two", "forty-three", "forty-four", "forty-five", "forty-six", "forty-seven", "forty-eight", "forty-nine",
-  "fifty", "fifty-one", "fifty-two", "fifty-hree", "fifty-four", "fifty-five", "fifty-six", "fifty-seven", "fifty-eight", "fifty-nine",
+  "fifty", "fifty-one", "fifty-two", "fifty-three", "fifty-four", "fifty-five", "fifty-six", "fifty-seven", "fifty-eight", "fifty-nine",
 ]
 
 const countdown = ref(TOMATO)
