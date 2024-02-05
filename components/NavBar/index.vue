79c1a6042d5678b84424235f968d1ede4ceb83bb
yuantianyu <yuantianyu@baidu.com>
2024-02-05 11:14:51 +0800

    Feat(navbar): fluent style navbar

--- components/NavBar/index.vue
+++ components/NavBar/index.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="relative h-18 w-full flex justify-between items-center">
-    <div id="links" class="flex gap-4">
-      <NuxtLink to="/">
+  <div class="relative h-18 px-4 flex items-center sm:(justify-center mt-1rem rounded-3xl) shadow-nsm  bg-gradient-to-br from-light-100 to-gray-100/50 dark:(from-purple-400/20 to-blue-300/20)">
+    <div id="links" class="flex items-center gap-4">
+      <!-- <NuxtLink to="/">
         <div class="group w-12 h-12 flex-center rounded-1rem transition-all transform active:(scale-95)">
-          <Icon name="fa6-solid:circle-notch" class="rotating text-2xl group-hover:(text-indigo-500)" />
+          <Icon name="fa6-solid:circle-notch" class="text-2xl group-hover:(text-indigo-500)" />
         </div>
-      </NuxtLink>
-      <NavBarButtonB icon="" to="/playground" tooltip="???" :activated="routeActivated" />
+      </NuxtLink> -->
+      <NavBarButtonB icon="fa6-solid:house" to="/" tooltip="Home" :activated="routeActivated" />
     </div>
     <div v-show="width > 640" id="links" class="flex gap-4 px-3">
+      <NuxtLink class="h-12 border-r" to="/playground"></NuxtLink>
       <NavBarButtonB icon="fa6-solid:vial" to="/projects" tooltip="Projects" :activated="routeActivated" />
       <NavBarButtonB icon="fa6-solid:feather-pointed" to="/article" tooltip="Articles" :activated="routeActivated" />
       <NavBarButtonB icon="fa6-solid:camera-retro" to="/gallery" tooltip="Gallery" :activated="routeActivated" />
       <NavBarButtonB icon="fa6-solid:icons" to="/components" tooltip="Compoents" :activated="routeActivated" />
       <NavBarButtonB icon="fa6-solid:user-tie" to="/about" tooltip="About" :activated="routeActivated" />
+      <div class="h-12 border-r"></div>
     </div>
-    <div v-show="width > 640" id="links" class="flex items-center gap-4 pl-3">
+    <div v-show="width > 640" id="links" class="flex items-center gap-4">
+      <NavBarColorSwitch />
       <NavBarButtonB icon="fa6-brands:git" to="https://github.com/paperplane110" tooltip="Github" target="_blank"
         :activated="routeActivated" />
-      <NavBarColorSwitch />
     </div>
     <div v-show="width <= 640" class="absolute right-2 z-50">
       <NavBarDropDown />
